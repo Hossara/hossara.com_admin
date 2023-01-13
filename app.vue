ada2a39ac3a83fb0fd5bfced5a07c267089a1ee1
Hossein Araghi <hoseinaraghi84@gmail.com>
2023-01-13 04:19:46 +0330

    Init and add starter dependencies and bootstrap

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout>
+    <NuxtLoadingIndicator color="#ffffff" :height="1"/>
+    <NuxtPage/>
+  </NuxtLayout>
+</template>
+
+<style lang="sass">
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap")
+@import bootstrap/scss/bootstrap
+</style>
